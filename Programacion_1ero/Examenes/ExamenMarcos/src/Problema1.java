52fb4cdc7398c99b0fbaa2fa5c9c2f94f4fe1f66
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2024-09-03 12:16:54 +0200

    SUBIDO DESDE CONSOLA

---
+++ Programacion_1ero/Examenes/ExamenMarcos/src/Problema1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Problema1 {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        
+
+
+
+
+    }
+}
