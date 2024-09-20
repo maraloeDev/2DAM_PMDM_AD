6beff1e258f9d4618dde7a121c381385d82b5e8e
Eduardo <martinsonsecaeduardo@gmail.com>
2024-09-20 19:23:43 +0200

    EJEMPLOS00-01

--- PMDM/Evaluacion1ra/Ejemplos/Ejemplo01_ComponentesBasicos/app/src/main/java/com/maraloedev/ejemplo01_componentesbasicos/MainActivity.kt
+++ PMDM/Evaluacion1ra/Ejemplos/Ejemplo01_ComponentesBasicos/app/src/main/java/com/maraloedev/ejemplo01_componentesbasicos/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.maraloedev.ejemplo01_componentesbasicos
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -12,15 +13,18 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.viewbinding.ViewBinding
 import com.maraloedev.ejemplo01_componentesbasicos.databinding.ActivityMainBinding
 
+
 lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        //setContentView(R.layout.activity_main)
+      //setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         val boton = binding.bRecuperarNombre
         val nombre = findViewById<EditText>(R.id.etNombreUsuario)
