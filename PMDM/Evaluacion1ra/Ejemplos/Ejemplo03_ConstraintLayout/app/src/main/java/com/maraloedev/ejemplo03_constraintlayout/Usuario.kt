41207cea7234b9352f8dd2d41143f6cf82bbe35b
Eduardo <martinsonsecaeduardo@gmail.com>
2024-10-14 10:54:30 +0200

    EJEMPLO CONSTRAINTS

---
+++ PMDM/Evaluacion1ra/Ejemplos/Ejemplo03_ConstraintLayout/app/src/main/java/com/maraloedev/ejemplo03_constraintlayout/Usuario.kt
@@ -0,0 +1,8 @@
+package com.maraloedev.ejemplo03_constraintlayout
+
+class Usuario(var nombre:String, var apellido1:String, var apellido2:String) {
+
+    override fun toString(): String {
+        return "Usuario(nombre='$nombre', apellido1='$apellido1', apellido2='$apellido2')"
+    }
+}
