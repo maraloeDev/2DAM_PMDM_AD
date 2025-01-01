c70cb8844f952da12f12874509e048ae6b57cb64
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2025-01-01 20:22:42 +0100

    EJEMPLOS

--- PMDM/Evaluacion2da/Ejemplos/Ejemplo07_Menus/app/src/main/java/com/maraloedev/ejemplo07_menus/MainActivity2.kt
+++ PMDM/Evaluacion2da/Ejemplos/Ejemplo07_Menus/app/src/main/java/com/maraloedev/ejemplo07_menus/MainActivity2.kt
@@ -1,19 +1,20 @@
 package com.maraloedev.ejemplo07_menus
 
 import android.os.Bundle
-import com.maraloedev.ejemplo07_menus.databinding.ActivityMain2Binding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : GestionMenus() {
-
-    lateinit var binding: ActivityMain2Binding
-
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMain2Binding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        registerForContextMenu(binding.llFondoActivity2)
-
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
