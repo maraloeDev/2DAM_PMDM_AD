a3f7c5a37e7e02f952247bcac3397c4d9c8f0ade
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2025-01-24 12:10:17 +0100

    Ejemplo07_relaciones

--- Acceso a Datos/Evaluacion_2/WorkSpace2daEvaluacionSpring/demo-6/src/main/java/com/maraloedev/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.maraloedev.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.maraloedev.entities.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
