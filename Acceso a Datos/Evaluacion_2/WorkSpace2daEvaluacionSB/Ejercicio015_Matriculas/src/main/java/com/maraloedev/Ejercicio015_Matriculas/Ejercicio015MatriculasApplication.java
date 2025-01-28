14afe1459e9521b5810343c6d6e46d3d57988927
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2025-01-28 17:12:14 +0100

    DEMO7

---
+++ Acceso a Datos/Evaluacion_2/WorkSpace2daEvaluacionSB/Ejercicio015_Matriculas/src/main/java/com/maraloedev/Ejercicio015_Matriculas/Ejercicio015MatriculasApplication.java
@@ -0,0 +1,13 @@
+package com.maraloedev.Ejercicio015_Matriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio015MatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio015MatriculasApplication.class, args);
+	}
+
+}
---
+++ Acceso a Datos/Evaluacion_2/WorkSpace2daEvaluacionSB/Ejercicio015_Matriculas/src/test/java/com/maraloedev/Ejercicio015_Matriculas/Ejercicio015MatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maraloedev.Ejercicio015_Matriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio015MatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
