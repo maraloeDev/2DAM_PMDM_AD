871b4dce9253102ec5374dd94243b3ac60bf5b78
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2025-01-29 10:48:01 +0100

    EJERCICIO018_SKILLS

---
+++ Acceso a Datos/Evaluacion_2/WorkSpace2daEvaluacionSB/Ejercicio018_Skills/src/main/java/com/maraloedev/Ejercicio018_Skills/Ejercicio018SkillsApplication.java
@@ -0,0 +1,13 @@
+package com.maraloedev.Ejercicio018_Skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio018SkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio018SkillsApplication.class, args);
+	}
+
+}
---
+++ Acceso a Datos/Evaluacion_2/WorkSpace2daEvaluacionSB/Ejercicio018_Skills/src/test/java/com/maraloedev/Ejercicio018_Skills/Ejercicio018SkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maraloedev.Ejercicio018_Skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio018SkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
